d7d374a84fb8ba8f397e76b994c702f55375e107
pwnedpixel <s1l3nt.n00bz@gmail.com>
2017-01-21 11:47:53 -0500

    added readImgDescTest.py

--- src/GoogleSpeechReadTest.py
+++ src/GoogleSpeechReadTest.py
@@ -3,9 +3,11 @@ from gtts import gTTS
 from pygame import mixer
 import subprocess
 
-tts = gTTS(text='This is a Test. Do not panic.', lang='en')
+
 print("STart")
+tts = gTTS(text='This is a Test. Do not panic.', lang='en')
 tts.save("speech.mp3")
 print("End")
 
+#subprocess.call(["/bin/mv","/tmp/a","/tmp/b"])
 subprocess.Popen(['mpg123', '-q', "speech.mp3"]).wait()
